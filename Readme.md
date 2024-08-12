7c413730af7b45ca2dda99874e100ab380a906f3
jeons <etoile929@naver.com>
2024-08-12 21:19:22 +0900

    자바스크립트에서 타입스크립트로 변환중

---
+++ Readme.md
@@ -0,0 +1,11 @@
+## 프로젝트 생성 
+`npx create-expo-app -t expo-template-blank-typescript`<br>
+기존 javascript로 구성한 컴포넌트를 typescript로 변환한다.(js파일을 모두 tsx파일로 변환)<br>
+
+## typescript 테스트 
+
+json의 scripts에 test:typescript:"tsc"추가  ->`npm run test:typescript`실행<br>
+
+## 아이콘 설치
+
+` npm install @expo/vector-icons `<br>
---
+++ src/components/Header/HeaderButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "react-native";
+import { Icon } from "../Icons";
+
+export class HeaderIcon extends React.Component {
+    render(){
+        return(
+            <Button onPress={this.props.onPress} title="ㅁ">
+                    <Icon name= {this.props.iconName} size= {28} color='black'/> 
+            </Button>
+        )
+    }
+}
